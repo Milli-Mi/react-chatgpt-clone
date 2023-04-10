ad0f01a245ff37a000dfa18051bb1d44ef165855
Milli-Mi <87952989+Milli-Mi@users.noreply.github.com>
2023-04-10 16:12:03 +0300

    Update App.js
    adding <nav>

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ const App = () => {
       <section className="side-bar">
         <button>+ New chat</button>
         <ul className="history"></ul>
+        <nav></nav>
       </section>
       <section className="main"></section>
     </div>
