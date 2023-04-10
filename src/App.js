a51ef7ecfc00850c5b98f047152747c8738c0603
Milli-Mi <87952989+Milli-Mi@users.noreply.github.com>
2023-04-10 16:07:16 +0300

    Update App.js
    adding <button> +New chat

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ const App = () => {
   return (
     <div className="app">
       <section className="side-bar">
-        
+        <button>+ New chat</button>
       </section>
       <section className="main"></section>
     </div>
