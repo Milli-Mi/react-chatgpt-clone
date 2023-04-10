b6ac3deb8b2ec299297a7b6d2a62948939c51573
Milli-Mi <87952989+Milli-Mi@users.noreply.github.com>
2023-04-10 16:05:54 +0300

    Update App.js
    adding two <section>

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@
 const App = () => {
   return (
     <div className="app">
-      
+      <section className="side-bar">
+        
+      </section>
+      <section className="main"></section>
     </div>
   )
 }
