5112cc1f7fdbb1708b1c38452c1de5cb145bdc6b
Milli-Mi <87952989+Milli-Mi@users.noreply.github.com>
2023-04-12 18:01:55 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,13 @@ const App = () => {
       <section className="main">
         <h1>TanyaGPT</h1>
         <ul className="feed"></ul>
-        <div className="bottom-section"></div>
+        <div className="bottom-section">
+          <div className="input-container">
+            <input/>
+            <div id="submit">➢ </div>
+          </div>
+          <p className="info">We’ve trained a model called ChatGPT which interacts in a conversational way. The dialogue format makes it possible for ChatGPT to answer followup questions, admit its mistakes, challenge incorrect premises, and reject inappropriate requests.</p>
+        </div>
       </section>
     </div>
   )
